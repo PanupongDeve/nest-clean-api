c013c4463238441d3fd0ce21ee3dfaf222405ce4
Panupong Chamsomboon <5635512110@psu.ac.th>
2020-07-14 05:46:05 +0000

    change mock logic to mock class

--- src/core/dataSource/mocks/usersMock.ts
+++ src/core/dataSource/mocks/usersMock.ts
@@ -1,7 +1,106 @@
-let usersMock: any[] = [
-    { id: 0, firstName: 'John', lastName: 'Dohn' },
-    { id: 1, firstName: 'Jessica', lastName: 'Bob' }
-]
+import { User } from '../../entities/users/User';
+import { utils } from '../../utils';
 
+
+class UsersMock {
+    private users: Array<any> = [
+        { id: 0, firstName: 'John', lastName: 'Dohn' },
+        { id: 1, firstName: 'Jessica', lastName: 'Bob' }
+    ];
+
+    private static instance: UsersMock;
+
+    private constructor() {
+   
+    }
+
+    public static getInstance(): UsersMock {
+        if (!UsersMock.instance) {
+            UsersMock.instance = new UsersMock();
+        }
+
+        return UsersMock.instance;
+    }
+
+
+    private mappingFromDataSourceToUsers(users: any[]): User[] {
+        const filteredUsers = users.filter((user) => user !== null);
+
+        const mappedUsers: User[] = filteredUsers.map((user) => {
+            return new User(user);
+        });
+
+        return mappedUsers;
+    }
+
+
+    private filterUsersById(id: string, users: User[]): User {
+        const user: User = users.filter((item) => Number(item.id) === Number(id))[0];
+
+        return user;
+    }
+
+    private updateUserMockAttribute(user: User, userUpdated: User): User {
+        user.firstName = userUpdated.firstName ? userUpdated.firstName : user.firstName;
+        user.lastName = userUpdated.lastName ? userUpdated.lastName: user.lastName;
+
+        return user;
+    }
+
+    getUsers() {
+        const usersReponse = this.users.filter(user => user !== null)
+        return usersReponse;
+    }
+
+    getUserById(id: string): User {
+        const users: User[] =  this.mappingFromDataSourceToUsers(this.users);
+        const user: User = this.filterUsersById(id, users);
+        return user;
+    }
+
+    createUser(user: any): User[] {
+        user.id = utils.generateIdFromArray(this.users);
+        const userMapped: User = new User(user);
+        this.users.push(userMapped)
+        const users = this.getUsers();
+        return users;
+    }
+
+    updateByid(id: string, userData: any): User[] {
+        const userMapped: User = new User(userData);
+        const users: User[] =  this.mappingFromDataSourceToUsers(this.users);
+
+        let usersUpdated: User[] = users.map((user) => {
+            if (Number(user.id) === Number(id)) {
+                const indexId = Number(id);
+                let userUpdated:User = this.updateUserMockAttribute(users[indexId], userMapped);
+                this.users[indexId] = userUpdated;
+                return userUpdated;
+            } else {
+                return user;
+            }
+        })
+
+        return usersUpdated;
+    }
+
+    deleteByid(id: string): User[] {
+        const users: User[] =  this.mappingFromDataSourceToUsers(this.users);
+        let userUpdated: User[] = users.map((user) => {
+            if (Number(user.id) === Number(id)) {
+                const indexId = Number(id);
+                this.users[indexId] = null;
+                user = null;
+                return user;
+            } else {
+                return user;
+            }
+        });
+
+        return userUpdated;
+    }
+}
+
+const usersMock: UsersMock = UsersMock.getInstance();
 
 export { usersMock }
--- src/core/repositories/users/UserRepository.ts
+++ src/core/repositories/users/UserRepository.ts
@@ -4,36 +4,11 @@ import { DataSource } from '../../constant/Enum';
 import { utils } from '../../utils';
 
 class UserRepository {
-
-    private mappingFromDataSourceToUsers(users: any[]): User[] {
-        const filteredUsers = users.filter((user) => user !== null);
-
-        const mappedUsers: User[] = filteredUsers.map((user) => {
-            return new User(user);
-        });
-
-        return mappedUsers;
-    }
-
-
-    private filterUsersById(id: string, users: User[]): User {
-        const user: User = users.filter((item) => Number(item.id) === Number(id))[0];
-
-        return user;
-    }
-
-    private updateUserMockAttribute(user: User, userUpdated: User): User {
-        user.firstName = userUpdated.firstName ? userUpdated.firstName : user.firstName;
-        user.lastName = userUpdated.lastName ? userUpdated.lastName: user.lastName;
-
-        return user;
-    }
-
     public async getUsers(): Promise<User[]> {
         const dataSourceSelected = DataSource.Mock;
 
         if (dataSourceSelected === DataSource.Mock) {
-            const users = this.mappingFromDataSourceToUsers(usersMock)
+            const users = usersMock.getUsers();
             return users;
         }   
     }
@@ -41,10 +16,8 @@ class UserRepository {
     public async getUserById(id: string): Promise<User> {
         const dataSourceSelected = DataSource.Mock;
 
-        if (dataSourceSelected === DataSource.Mock) {
-            const users: User[] =  this.mappingFromDataSourceToUsers(usersMock);
-            const user: User = this.filterUsersById(id, users);
-
+        if (dataSourceSelected === DataSource.Mock) { 
+            const user: User = usersMock.getUserById(id);
             return user;
         }
     }
@@ -54,10 +27,7 @@ class UserRepository {
         const dataSourceSelected = DataSource.Mock;
 
         if (dataSourceSelected === DataSource.Mock) {
-            user.id = utils.generateIdFromArray(usersMock);
-            const userMapped: User = new User(user);
-            usersMock.push(userMapped);
-            const users = this.getUsers();
+            const users = usersMock.createUser(user);
             return users;
         }
 
@@ -68,20 +38,7 @@ class UserRepository {
         const dataSourceSelected = DataSource.Mock;
 
         if (dataSourceSelected === DataSource.Mock) {
-            const userMapped: User = new User(userData);
-            const users: User[] =  this.mappingFromDataSourceToUsers(usersMock);
-
-            let usersUpdated: User[] = users.map((user) => {
-                if (Number(user.id) === Number(id)) {
-                    const indexId = Number(id);
-                    let userUpdated:User = this.updateUserMockAttribute(users[indexId], userMapped);
-                    usersMock[indexId] = userUpdated;
-                    return userUpdated;
-                } else {
-                    return user;
-                }
-            })
-
+            let usersUpdated = usersMock.updateByid(id, userData);
             return usersUpdated;
         }
 
@@ -92,18 +49,7 @@ class UserRepository {
         const dataSourceSelected = DataSource.Mock;
         
         if (dataSourceSelected === DataSource.Mock) {
-            const users: User[] =  this.mappingFromDataSourceToUsers(usersMock);
-            let userUpdated: User[] = users.map((user) => {
-                if (Number(user.id) === Number(id)) {
-                    const indexId = Number(id);
-                    usersMock[indexId] = null;
-                    user = null;
-                    return user;
-                } else {
-                    return user;
-                }
-            });
-
+            let userUpdated = usersMock.deleteByid(id);
             return userUpdated;
         }
         
